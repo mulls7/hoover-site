1c44619b6db56fdae0a99e99af1a0ea1bdf6f912
mulls7 <59904900+mulls7@users.noreply.github.com>
2023-04-10 22:12:28 -0400

    scroll trigger

--- dist/scripts.js
+++ dist/scripts.js
@@ -38,3 +38,39 @@ console.log("he")
     });
   });
   */
+ gsap.registerPlugin(ScrollTrigger);
+ gsap.from('.scroll', {
+  y: 100,
+  opacity: 0,
+  start: "top center",
+  duration: 2,
+  ease: 'power4',
+  scrollTrigger: {
+    trigger: '.scroll',
+    markers: true,
+  }
+ })
+
+ gsap.from('.scroll2', {
+  y: 100,
+  opacity: 0,
+  start: 'top center',
+  duration: 2,
+  ease: 'power4',
+  scrollTrigger: {
+    trigger: '.scroll2',
+    markers: true,
+  }
+ })
+
+ gsap.from('.scroll3', {
+  x: -100,
+  start: 'top center',
+  duration: 2,
+  ease: 'power4',
+  scrollTrigger: {
+    trigger: '.scroll3',
+    markers: true
+    
+  }
+ })
